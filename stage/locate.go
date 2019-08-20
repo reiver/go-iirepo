de2f3aabde9c36ed4da61a643d041db44aa23f1e
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-08-20 16:36:19 -0700

    iirepo_stage.Locate()

--- stage/locate.go
+++ stage/locate.go
@@ -2,6 +2,7 @@ package iirepo_stage
 
 import (
 	"github.com/reiver/go-iirepo"
+	"github.com/reiver/go-iirepo/logger"
 )
 
 // Locate returns the path to the repo's stage for ‘path’.
@@ -31,12 +32,19 @@ import (
 //
 //	"/home/joeblow/workspaces/myproject/.ii/stage"
 func Locate(path string) (stagepath string, err error) {
+
+	iirepo_logger.Debugf("iirepo_stage.Locate(%q): begin", path)
+
 	repopath, err := iirepo.Locate(path)
 	if nil != err {
 		return "", err
 	}
+	iirepo_logger.Debugf("iirepo_stage.Locate(%q): repopath = %q", path, repopath)
 
 	stagepath = Path(repopath)
+	iirepo_logger.Debugf("iirepo_stage.Locate(%q): stagepath = %q", path, stagepath)
+
+	iirepo_logger.Debugf("iirepo_stage.Locate(%q): end", path)
 
 	return stagepath, nil
 }
