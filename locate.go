d54c49616271f3a88bbc1b8f5be2996cb11a1ccc
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-08-20 11:03:37 -0700

    iirepo.LocateRoot()

--- locate.go
+++ locate.go
@@ -68,3 +68,40 @@ func Locate(path string) (repopath string, err error) {
 
 	return "", fmt.Errorf("iirepo: repo not found for %s", path)
 }
+
+// LocateRoot returns the path to the root for ‘path’.
+//
+// For example, if name of the repo directory is “.ii/”, and the directory structure looks like:
+//
+//	/home/joeblow/workspaces/
+//	/home/joeblow/workspaces/myproject/
+//	/home/joeblow/workspaces/myproject/.ii    <--------- Note the repo directory here.
+//	/home/joeblow/workspaces/myproject/apple.go
+//	/home/joeblow/workspaces/myproject/banana.go
+//	/home/joeblow/workspaces/myproject/cherry.go
+//	/home/joeblow/workspaces/myproject/date.go
+//	/home/joeblow/workspaces/myproject/numbers/
+//	/home/joeblow/workspaces/myproject/numbers/one.go
+//	/home/joeblow/workspaces/myproject/numbers/two.go
+//	/home/joeblow/workspaces/myproject/numbers/three.go
+//	/home/joeblow/workspaces/myproject/emoji/
+//	/home/joeblow/workspaces/myproject/emoji/grinning.go
+//	/home/joeblow/workspaces/myproject/emoji/slightlysmiling.go
+//
+// Then calling:
+//
+//	location, err := iirepo.LocateRoot("/home/joeblow/workspaces/myproject/numbers")
+//
+// Would return a value for ‘location’ of:
+//
+//	"/home/joeblow/workspaces/myproject"
+func LocateRoot(path string) (rootpath string, err error) {
+	repopath, err := Locate(path)
+	if nil != err {
+		return "", err
+	}
+
+	rootpath = filepath.Dir(repopath)
+
+	return rootpath, nil
+}
