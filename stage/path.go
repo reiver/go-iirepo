bbe5cd5bb3aa8cdec2b6ff0619fe3c20d8989139
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-08-20 16:39:11 -0700

    iirepo_stage.Path()

--- stage/path.go
+++ stage/path.go
@@ -2,14 +2,22 @@ package iirepo_stage
 
 import (
 	"github.com/reiver/go-iirepo"
+	"github.com/reiver/go-iirepo/logger"
 
 	"path/filepath"
 )
 
 func Path(grandparent string) string {
+
+	iirepo_logger.Debugf("iirepo_stage.Path(%q): begin", grandparent)
+
 	repopath := iirepo.Path(grandparent)
+	iirepo_logger.Debugf("iirepo_stage.Path(%q): repopath = %q", grandparent, repopath)
 
 	path := filepath.Join(repopath, Name())
+	iirepo_logger.Debugf("iirepo_stage.Path(%q): path = %q", grandparent, path)
+
+	iirepo_logger.Debugf("iirepo_stage.Path(%q): end", grandparent)
 
 	return path
 }
