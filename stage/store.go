72745ac0c5d4d036c8d88441259cff24bf693727
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-08-21 15:45:38 -0700

    iirepo_stage.Store() -> iirepo_stage.StoreCopy() & iirepo_stage.StoreOriginal()

--- stage/store.go
+++ stage/store.go
@@ -9,38 +9,99 @@ import (
 	"path/filepath"
 )
 
-// Store copied the file at ‘path’ to the stage.
-func Store(path string) (err error) {
+func storewhere(path string) (srcpath string, dstpath string, dstpathdir string, err error) {
 
-	iirepo_logger.Debugf("iirepo_stage.Store(%q): begin", path)
+	iirepo_logger.Debugf("iirepo_stage.storewhere(%q): begin", path)
 
-	var srcpath string
+	//var srcpath string
 	{
 		var err error
 
 		srcpath, err = filepath.Abs(path)
 		if nil != err {
-			return err
+			return "", "", "", err
 		}
 	}
-	iirepo_logger.Debugf("iirepo_stage.Store(%q): srcpath = %q", path, srcpath)
+	iirepo_logger.Debugf("iirepo_stage.storewhere(%q): srcpath = %q", path, srcpath)
 
-	var dstpath string
+	//var dstpath string
 	{
 		var err error
 
 		dstpath, err = StagedPath(path)
 		if nil != err {
-			return err
+			return "", "", "", err
 		}
 	}
-	iirepo_logger.Debugf("iirepo_stage.Store(%q): dstpath = %q", path, dstpath)
+	iirepo_logger.Debugf("iirepo_stage.storewhere(%q): dstpath = %q", path, dstpath)
 
-	var dstpathdir string
+	//var dstpathdir string
 	{
 		dstpathdir = filepath.Dir(dstpath)
 	}
-	iirepo_logger.Debugf("iirepo_stage.Store(%q): dstpathdir = %q", path, dstpathdir)
+	iirepo_logger.Debugf("iirepo_stage.storewhere(%q): dstpathdir = %q", path, dstpathdir)
+
+	iirepo_logger.Debugf("iirepo_stage.storewhere(%q): end", path)
+
+	return srcpath, dstpath, dstpathdir, nil
+}
+
+// Store moves the file at ‘path’ to the stage.
+//
+// See also: StoreCopy
+func StoreOriginal(path string) (err error) {
+
+	iirepo_logger.Debugf("iirepo_stage.StoreOriginal(%q): begin", path)
+
+	var srcpath    string
+	var dstpath    string
+	var dstpathdir string
+	{
+		var err error
+
+		srcpath, dstpath, dstpathdir, err = storewhere(path)
+		if nil != err {
+			return err
+		}
+	}
+	iirepo_logger.Debugf("iirepo_stage.StoreOriginal(%q): srcpath = %q", path, srcpath)
+	iirepo_logger.Debugf("iirepo_stage.StoreOriginal(%q): dstpath = %q", path, dstpath)
+	iirepo_logger.Debugf("iirepo_stage.StoreOriginal(%q): dstpathdir = %q", path, dstpathdir)
+
+	if err := os.MkdirAll(dstpathdir, os.ModePerm); nil != err {
+		return err
+	}
+
+	if err := os.Rename(srcpath, dstpath); nil != err {
+		return err
+	}
+
+	iirepo_logger.Debugf("iirepo_stage.StoreOriginal(%q): end", path)
+
+	return nil
+}
+
+// Store copies the file at ‘path’ to the stage.
+//
+// See also: StoreOriginal
+func StoreCopy(path string) (err error) {
+
+	iirepo_logger.Debugf("iirepo_stage.StoreCopy(%q): begin", path)
+
+	var srcpath    string
+	var dstpath    string
+	var dstpathdir string
+	{
+		var err error
+
+		srcpath, dstpath, dstpathdir, err = storewhere(path)
+		if nil != err {
+			return err
+		}
+	}
+	iirepo_logger.Debugf("iirepo_stage.StoreCopy(%q): srcpath = %q", path, srcpath)
+	iirepo_logger.Debugf("iirepo_stage.StoreCopy(%q): dstpath = %q", path, dstpath)
+	iirepo_logger.Debugf("iirepo_stage.StoreCopy(%q): dstpathdir = %q", path, dstpathdir)
 
 	var src *os.File
 	{
@@ -65,7 +126,7 @@ func Store(path string) (err error) {
 			return fmt.Errorf("iirepo: %q is not a regular file", srcpath)
 		}
 	}
-	iirepo_logger.Debugf("iirepo_stage.Store(%q): src file opened", path)
+	iirepo_logger.Debugf("iirepo_stage.StoreCopy(%q): src file opened", path)
 
 	var dst *os.File
 	{
@@ -86,7 +147,7 @@ func Store(path string) (err error) {
 			}
 		}()
 	}
-	iirepo_logger.Debugf("iirepo_stage.Store(%q): dst file opened", path)
+	iirepo_logger.Debugf("iirepo_stage.StoreCopy(%q): dst file opened", path)
 
 	{
 		var buffer [64*8]byte // The 64 comes from the typical length of a ‘cache line’.
@@ -110,11 +171,11 @@ func Store(path string) (err error) {
 			}
 		}
 	}
-	iirepo_logger.Debugf("iirepo_stage.Store(%q): copy from src to dst completed", path)
+	iirepo_logger.Debugf("iirepo_stage.StoreCopy(%q): copy from src to dst completed", path)
 
 //@TODO: add some checks to make sure file was copied OK.
 
-	iirepo_logger.Debugf("iirepo_stage.Store(%q): end", path)
+	iirepo_logger.Debugf("iirepo_stage.StoreCopy(%q): end", path)
 
 	return nil
 }
