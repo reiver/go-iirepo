2975ca2bd82e81bc239e75ed1018d0d6ea0265cb
Charles Iliya Krempeaux <iamreiver@gmail.com>
2019-08-22 10:49:50 -0700

    iirepo_apps.List()

---
+++ apps/list.go
@@ -0,0 +1,41 @@
+package iirepo_apps
+
+import (
+	"os"
+	"path/filepath"
+)
+
+func List(path string) ([][]string, error) {
+
+	iirepo_logger.Debugf("iirepo_apps.List(%q): begin", path)
+
+	appspath, err := Locate(path)
+	if nil != err {
+		return nil, err
+	}
+	iirepo_logger.Debugf("iirepo_apps.List(%q): appspath = %q", path, appspath)
+
+	var apps [][]string
+
+	err = filepath.Walk(appspath, func(apppath string, info os.FileInfo, err error) error {
+		if nil != err {
+			return err
+		}
+
+		appName := filepath.Base(apppath)
+		iirepo_logger.Debugf("iirepo_apps.List(%q): app-name = %q", path, appName)
+
+		app := []string{appName}
+
+		apps = append(apps, app)
+
+		return nil
+	})
+	if nil != err {
+		return nil, err
+	}
+
+	iirepo_logger.Debugf("iirepo_apps.List(%q): end", path)
+
+	return apps, nil
+}
